6c1ae63af1fba10ba133450beb61ce03484708cd
kunxl-gg <tiwari.25@iitj.ac.in>
2024-04-06 20:07:46 +0530

    GAME: Added assimp to the project
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 int main() {
+
+    Assimp::Importer importer;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
---
+++ src/utils/model.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+class Model {
+public: 
+    Model();
+    ~Model();
+    void Draw();
+
+private:
+    std::string directory;
+
+
+};
